304fe598927433f873120c4105d0488c83a60858
jaspreetsanghra <jaspreet.sanghra@gmail.com>
2022-03-22 23:04:49 -0400

    remove long merge commit message

--- Worker/Models/Commit.cs
+++ Worker/Models/Commit.cs
@@ -3,6 +3,24 @@ namespace Worker.Models;
 public class Commit
 {
     public string Author = "";
-    public string Message = "";
+    private string _message = "";
+    public string Message
+    {
+        get
+        {
+            return _message;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value) && value.StartsWith("Merge pull request #") && value.IndexOf("from") > 0)
+            {
+                _message = value.Substring(0, value.IndexOf("from"));
+            }
+            else
+            {
+                _message = value;
+            }
+        }
+    }
     public string UrlLink = "";
 }
