db853d403f7f0a6537b8d5f4678e6efaa1aa6f7a
jaspreetsanghra <jaspreet.sanghra@gmail.com>
2022-03-23 21:46:46 -0400

    Try steps

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -103,66 +103,56 @@ public class Worker : BackgroundService
             var stepsObj = JObject.Parse(STEPS);
             foreach (var o in stepsObj)
             {
-                Console.WriteLine(o.Key);
-                Console.WriteLine(o.Value);
+                formattedSteps = "";
+                {
+                    var value = o.Value?["outcome"]?.ToString() ?? "";
+                    Console.WriteLine(value);
+                    formattedSteps = formattedSteps + "  \n`" + o.Key + "` - `" + $"{(value == "success" ? "pass" : "fail")}" + "`";
 
+                }
             }
-            // var stepsDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(STEPS);
-            // if (stepsDictionary != null && stepsDictionary.Keys.Count > 0)
-            // {
-            //     formattedSteps = "";
-            //     foreach (var k in stepsDictionary.Keys)
-            //     {
-            //         var currentVal = stepsDictionary[k];
-            //         var parsedObj = JObject.Parse(currentVal);
-            //         var value = parsedObj?["outcome"]?.ToString() ?? "";
-            //         formattedSteps = formattedSteps + "  \n`" + k + "` - `" + $"{(value == "success" ? "pass" : "fail")}" + "`";
-            //     }
-            // }
 
-        }
+            Console.WriteLine($"[BRANCH_NAME] Value:[{BRANCH_NAME}]");
 
-        Console.WriteLine($"[BRANCH_NAME] Value:[{BRANCH_NAME}]");
+            Console.WriteLine($"[compareLink] Value:[{compareLink}]");
+            Console.WriteLine($"[repositoryLink] Value:[{repositoryLink}]");
 
-        Console.WriteLine($"[compareLink] Value:[{compareLink}]");
-        Console.WriteLine($"[repositoryLink] Value:[{repositoryLink}]");
+            Console.WriteLine($"[commits] Value:[{JsonConvert.SerializeObject(commits)}]");
+            Console.WriteLine($"[TITLE] Value:[{TITLE}]");
+            Console.WriteLine($"[DESCRIPTION] Value:[{DESCRIPTION}]");
+            Console.WriteLine($"[REPOSITORY_NAME] Value:[{REPOSITORY_NAME}]");
+            Console.WriteLine($"[WORKFLOW_NAME] Value:[{WORKFLOW_NAME}]");
+            Console.WriteLine($"[GITHUB_EVENT] Value:[{GITHUB_EVENT}]");
 
-        Console.WriteLine($"[commits] Value:[{JsonConvert.SerializeObject(commits)}]");
-        Console.WriteLine($"[TITLE] Value:[{TITLE}]");
-        Console.WriteLine($"[DESCRIPTION] Value:[{DESCRIPTION}]");
-        Console.WriteLine($"[REPOSITORY_NAME] Value:[{REPOSITORY_NAME}]");
-        Console.WriteLine($"[WORKFLOW_NAME] Value:[{WORKFLOW_NAME}]");
-        Console.WriteLine($"[GITHUB_EVENT] Value:[{GITHUB_EVENT}]");
+            Console.WriteLine($"[STEPS] Value:[{STEPS}]");
+            Console.WriteLine($"[JOB] Value:[{JOB}]");
 
-        Console.WriteLine($"[STEPS] Value:[{STEPS}]");
-        Console.WriteLine($"[JOB] Value:[{JOB}]");
+            Console.WriteLine($"[NEEDS] Value:[{NEEDS}]");
 
-        Console.WriteLine($"[NEEDS] Value:[{NEEDS}]");
-
-        var formattedDesc = DESCRIPTION;
-        if (commits != null && commits.Count > 0)
-        {
-            formattedDesc = formattedDesc + "  \n  " + "**Commits:**";
-            int MAX_COMMITS = 10;
-            for (int i = 0; i < MAX_COMMITS && i < commits.Count; i++)
+            var formattedDesc = DESCRIPTION;
+            if (commits != null && commits.Count > 0)
             {
-                var currentCommit = commits[i];
-                formattedDesc = formattedDesc + "\n+ " + $"[{currentCommit.Author} - {currentCommit.Message}]({currentCommit.UrlLink})";
+                formattedDesc = formattedDesc + "  \n  " + "**Commits:**";
+                int MAX_COMMITS = 10;
+                for (int i = 0; i < MAX_COMMITS && i < commits.Count; i++)
+                {
+                    var currentCommit = commits[i];
+                    formattedDesc = formattedDesc + "\n+ " + $"[{currentCommit.Author} - {currentCommit.Message}]({currentCommit.UrlLink})";
+                }
+                if (commits.Count > MAX_COMMITS)
+                {
+                    formattedDesc = formattedDesc + "\n+ " + $"{(commits.Count - MAX_COMMITS)} more...";
+                }
             }
-            if (commits.Count > MAX_COMMITS)
+
+
+
+            MessageBody messageCard = new MessageBody()
             {
-                formattedDesc = formattedDesc + "\n+ " + $"{(commits.Count - MAX_COMMITS)} more...";
-            }
-        }
-
-
-
-        MessageBody messageCard = new MessageBody()
-        {
-            Title = !string.IsNullOrWhiteSpace(TITLE) ? TITLE : $"[{REPOSITORY_NAME}] - [{WORKFLOW_NAME}]",
-            Text = formattedDesc,
-            ThemeColor = jobRunWasSuccessful ? CardColours.GOOD : CardColours.DANGER,
-            Sections = new List<MessageBody.Section>(){
+                Title = !string.IsNullOrWhiteSpace(TITLE) ? TITLE : $"[{REPOSITORY_NAME}] - [{WORKFLOW_NAME}]",
+                Text = formattedDesc,
+                ThemeColor = jobRunWasSuccessful ? CardColours.GOOD : CardColours.DANGER,
+                Sections = new List<MessageBody.Section>(){
                             new MessageBody.Section(){
                                 Facts = new List<MessageBody.Fact>(){
                                     new MessageBody.Fact(){Name="Result", Value=jobRunWasSuccessful ? "PASSED" : "FAILED"},
@@ -172,38 +162,39 @@ public class Worker : BackgroundService
                                 }
                             }
                         },
-            Actions = new List<MessageBody.Action>(){
+                Actions = new List<MessageBody.Action>(){
                             new MessageBody.Action(){DisplayName= "Repository", Targets = new List<MessageBody.ActionTarget>(){ new MessageBody.ActionTarget() {UriLink=repositoryLink}}},
                             new MessageBody.Action(){DisplayName= "Compare", Targets = new List<MessageBody.ActionTarget>(){ new MessageBody.ActionTarget() {UriLink=compareLink}}},
                             new MessageBody.Action(){DisplayName= "Workflow", Targets = new List<MessageBody.ActionTarget>(){ new MessageBody.ActionTarget() {UriLink=$"{repositoryLink}/actions/{(!string.IsNullOrWhiteSpace(GITHUB_RUN_ID) ? $"runs/{GITHUB_RUN_ID}" : "")}"}}}
                         }
-        };
-        if (!string.IsNullOrWhiteSpace(formattedSteps))
-        {
-            messageCard.Sections[0].Facts.Add(new MessageBody.Fact()
+            };
+            if (!string.IsNullOrWhiteSpace(formattedSteps))
             {
-                Name = "Steps",
-                Value = formattedSteps
-            });
+                messageCard.Sections[0].Facts.Add(new MessageBody.Fact()
+                {
+                    Name = "Steps",
+                    Value = formattedSteps
+                });
+            }
+
+            try
+            {
+                var cardJson = JsonConvert.SerializeObject(messageCard);
+                _logger.LogDebug("Sending notification for test [{title}]. Full Card: [{details}]", messageCard.Title, cardJson);
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                StringContent httpContent = new StringContent(cardJson, System.Text.Encoding.UTF8, "application/json");
+
+                await client.PostAsync(INPUT_WEBHOOK, httpContent);
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to send failure notification");
+            }
+            _applicationLifetime.StopApplication();
+
         }
-
-        try
-        {
-            var cardJson = JsonConvert.SerializeObject(messageCard);
-            _logger.LogDebug("Sending notification for test [{title}]. Full Card: [{details}]", messageCard.Title, cardJson);
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            StringContent httpContent = new StringContent(cardJson, System.Text.Encoding.UTF8, "application/json");
-
-            await client.PostAsync(INPUT_WEBHOOK, httpContent);
-
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Failed to send failure notification");
-        }
-        _applicationLifetime.StopApplication();
-
     }
 }
